6ab60990edece90c36538942710be369a9f01824
Hannah Alverson <alverson98@yahoo.com>
2022-08-12 14:33:41 -0400

    created variables for form results & save to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,13 @@ $("").text(today.format("MMM Do, YYYY"));
 
 var dateTimeFormat = today.format("dddd, MMMM Do YYYY, h:mm:ss a");
 $("#date-time").text(dateTimeFormat);
+
+var projectName = document.getElementById("project-name");
+var projectType = document.getElementById("project-type");
+var dueDate = document.getElementById("due-date");
+var hourlyRate = document.getElementById("hourly-rate");
+
+localStorage.setItem("name", projectName);
+localStorage.setItem("type", projectType);
+localStorage.setItem("date", dueDate);
+localStorage.setItem("rate", projectType);
